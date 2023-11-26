65ed373aefb308f4506a4a24b9b5aa0ddfef3a25
Nanami <82437383+Nanami-Hotta@users.noreply.github.com>
2023-11-26 14:21:27 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -14,6 +14,19 @@ $(function() {
     }
   });
 
+  // loading
+
+  $(function () {
+    function end_loader() {
+      $('.loader').fadeOut(800);
+    }
+    $(window).on('load', function () {
+      setTimeout(function () {
+        end_loader();
+      }, 3000)
+    })
+  })
+
   // Window Resize Function
 
   $(window).resize(function(){
